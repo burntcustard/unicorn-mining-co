0d585ee08a70e769b11478d6e0e8d4fdfce21237
burntcustard <burntcustard@gmail.com>
2026-08-28 10:02:43 +0100

    Base indicator distance off edge of screen instead of center of screen

--- src/ui/indicators.js
+++ src/ui/indicators.js
@@ -22,12 +22,16 @@ export const renderIndicators = (game, targets, color, range) => {
       target.x - camera.x - game.width / 2,
       target.y - camera.y - game.height / 2,
     );
-    const dist = Math.hypot(offset.x, offset.y);
 
-    if (dist > range || (Math.abs(offset.x) < game.width / 2 + target.radius &&
-      Math.abs(offset.y) < game.height / 2 + target.radius)) return;
+    const dist = Math.max(
+      Math.abs(offset.x) - game.width / 2,
+      Math.abs(offset.y) - game.height / 2,
+    ) - target.radius;
 
-    const size = 10 - 9 * dist / range;
+    if (dist > range || dist < 0) return;
+
+    const indicatorsize = 10 - 9 * dist / range;
+
     const edge = offset.scale(Math.min(
       (uiWidth / 2 - 20) / Math.abs(offset.x),
       (uiHeight / 2 - 20) / Math.abs(offset.y),
@@ -37,9 +41,9 @@ export const renderIndicators = (game, targets, color, range) => {
     ctx.translate(uiWidth / 2 + edge.x, uiHeight / 2 + edge.y);
     ctx.rotate(Math.atan2(offset.y, offset.x));
     ctx.beginPath();
-    ctx.moveTo(size, 0);
-    ctx.lineTo(0, -size);
-    ctx.lineTo(0, size);
+    ctx.moveTo(indicatorsize, 0);
+    ctx.lineTo(0, -indicatorsize);
+    ctx.lineTo(0, indicatorsize);
     ctx.closePath();
     ctx.stroke();
     ctx.restore();
@@ -49,10 +53,10 @@ export const renderIndicators = (game, targets, color, range) => {
       scale: 1,
       alignCenter: true,
       color,
-      size: 0.5,
+      indicatorsize: 0.5,
       text: `${Math.round(dist)}m`,
       x: uiWidth / 2 + edge.x,
-      y: uiHeight / 2 + edge.y - (size + 10) * Math.sign(offset.y || -1),
+      y: uiHeight / 2 + edge.y - (indicatorsize + 10) * Math.sign(offset.y || -1),
     });
   });
 
