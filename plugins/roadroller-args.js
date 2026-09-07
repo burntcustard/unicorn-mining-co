810e93434dea4cea7e85879e4cb428c344141792
burntcustard <burntcustard@gmail.com>
2026-09-07 21:08:41 +0100

    New roadroller args to save a few bytes

--- plugins/roadroller-args.js
+++ plugins/roadroller-args.js
@@ -1,8 +1,8 @@
 export default {
-  numAbbreviations: 27,
-  recipLearningRate: 2090,
+  numAbbreviations: 32,
+  recipLearningRate: 2501,
   modelMaxCount: 4,
-  modelRecipBaseCount: 41,
+  modelRecipBaseCount: 40,
   precision: 16,
-  sparseSelectors: [0, 1, 2, 3, 5, 7, 11, 13, 26, 53, 57, 198],
+  sparseSelectors: [0, 1, 2, 3, 5, 6, 7, 11, 13, 42, 53, 281],
 };
