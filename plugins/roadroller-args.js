c350eee7361f48747b07101e9bf22941f06a932d
burntcustard <burntcustard@gmail.com>
2026-09-12 23:02:20 +0100

    New build but its not great

--- plugins/roadroller-args.js
+++ plugins/roadroller-args.js
@@ -1,10 +1,10 @@
 export default {
   numAbbreviations: 9,
   dynamicModels: 1,
-  pairRecipLearningRate: 426,
-  recipLearningRate: 1704,
+  pairRecipLearningRate: 540,
+  recipLearningRate: 1620,
   modelMaxCount: 4,
-  modelRecipBaseCount: 23,
+  modelRecipBaseCount: 28,
   precision: 15,
   sparseSelectors: [0, 1, 2, 3, 5, 6, 7, 11, 13, 21, 26, 42, 57, 195, 353],
   sse: true,
