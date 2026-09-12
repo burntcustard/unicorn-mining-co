3e97a6c13f5571f95b7c98714ee58dd230057531
burntcustard <burntcustard@gmail.com>
2026-09-12 23:43:13 +0100

    New build with new params

--- plugins/roadroller-args.js
+++ plugins/roadroller-args.js
@@ -1,11 +1,11 @@
 export default {
   numAbbreviations: 9,
   dynamicModels: 1,
-  pairRecipLearningRate: 540,
-  recipLearningRate: 1620,
+  pairRecipLearningRate: 428,
+  recipLearningRate: 1712,
   modelMaxCount: 4,
-  modelRecipBaseCount: 28,
+  modelRecipBaseCount: 20,
   precision: 15,
-  sparseSelectors: [0, 1, 2, 3, 5, 6, 7, 11, 13, 21, 26, 42, 57, 195, 353],
+  sparseSelectors: [0, 1, 2, 3, 5, 6, 7, 11, 13, 21, 26, 42, 57, 195, 305],
   sse: true,
 };
